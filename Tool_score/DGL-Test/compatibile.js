19586caf2c7bc8cbaf4e0c8acc2494eac5b634b7
xiongjiyuan@i-vsun.com <xiongjiyuan@i-vsun.com>
2016-10-24 18:04:07 +0800

    no message

--- Tool_score/DGL-Test/compatibile.js
+++ Tool_score/DGL-Test/compatibile.js
@@ -2,34 +2,36 @@
  * Created by xiongjiyuan on 2016/10/19.
  */
 
-var brower = document.getElementById('brower');
-console.log(brower);
+window.onload = function () {
+    var brower = document.getElementById('brower');
+    console.log(brower);
 
-var Sys = {};
-var ua = navigator.userAgent.toLowerCase();
-var s;
-(s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
-    (s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
-        (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
-            (s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
-                (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
+    var Sys = {};
+    var ua = navigator.userAgent.toLowerCase();
+    var s;
+    (s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
+        (s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
+            (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
+                (s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
+                    (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
 
 //以下进行测试
-if (Sys.ie) brower.innerText = 'IE: ' + Sys.ie;
-if (Sys.firefox) brower.innerText = 'Firefox: ' + Sys.firefox;
-if (Sys.chrome) brower.innerText = 'Chrome: ' + Sys.chrome;
-if (Sys.opera) brower.innerText = 'Opera: ' + Sys.opera;
-if (Sys.safari) brower.innerText = 'Safari: ' + Sys.safari;
+    if (Sys.ie) brower.innerText = 'IE: ' + Sys.ie +'程序员最讨厌的浏览器';
+    if (Sys.firefox) brower.innerText = 'Firefox: ' + Sys.firefox;
+    if (Sys.chrome) brower.innerText = 'Chrome: ' + Sys.chrome +'世界上最好的浏览器';
+    if (Sys.opera) brower.innerText = 'Opera: ' + Sys.opera;
+    if (Sys.safari) brower.innerText = 'Safari: ' + Sys.safari;
 
-//WebSQL
-var db = openDatabase('testDB', '1.0', 'Test DB', 2 * 1024 * 1024);
-if (db != null)
-    document.getElementById("result-WebSQL").innerHTML = ("当前浏览器支持 Web SQL Database");
-else
-    document.getElementById("result-WebSQL").innerHTML = ("当前浏览器不支持 Web SQL Database");
+//WebSQL（这货的兼容性有毒）
+    var db = openDatabase('testDB', '1.0', 'Test DB', 2 * 1024 * 1024);
+    if (db != null)
+        document.getElementById("result-WebSQL").innerHTML = ("当前浏览器支持 Web SQL Database");
+    else
+        document.getElementById("result-WebSQL").innerHTML = ("当前浏览器不支持 Web SQL Database，赶快下载一个chrome吧");
 
-//IndexedDB
-if (indexedDB)
-    document.getElementById("result-IndexedDB").innerHTML = ("当前浏览器支持 indexedDB");
-else
-    document.getElementById("result-IndexedDB").innerHTML = ("当前浏览器不支持 indexedDB");
+//IndexedDB（浏览器最新版本均可兼容）
+    if (indexedDB)
+        document.getElementById("result-IndexedDB").innerHTML = ("当前浏览器支持 indexedDB");
+    else
+        document.getElementById("result-IndexedDB").innerHTML = ("当前浏览器不支持 indexedDB，请把你的IE或者Safari升级到10，或者下载一个chrome吧");
+};
--- Tool_score/DGLboard.js
+++ Tool_score/DGLboard.js
@@ -2,26 +2,27 @@
  * Created by xiongjiyuan on 2016/10/9.
  */
 //div#initialize 使用了¥作为未来的数据库重构标识
-$(function() {
+$(function () {
+
     //生成轮次//生成得分//生成学校
     $("select[name=area]").change(createTurn).change(createScore).change(createSchool);
 
-    //生成轮次、对阵、分数、最佳辩手¥
+    //生成地区、轮次、对阵、分数、最佳辩手¥
     function createTurn() {
         $("select[name=turn]").children().remove();
         var area = $("select[name=area]").val();
-        var turn = { '01': 16, '02': 8, '03': 8 };
+        var turn = {"01": "16", "02": "8", "03": "8", "04": "5"};
         for (var i = 1; i < turn[area]; i++) {
-            $("select[name=turn]").append("<option>第" + NumberToChinese(i) + "轮</option>");
+            $("select[name=turn]").append('<option value=' + i + '>' + '第' + NumberToChinese(i) + '轮</option>');
         }
     }
 
-    //数据库标记¥
+    //数据库标记¥ 学校名单
     function createSchool() {
         $("select[name=school]").children().remove();
         var area = $("select[name=area]").val();
-        var school_begin = { '01': '101', '02': '201', '03': '301' };
-        var school_end = { '01': '117', '02': '205', '03': '309' };
+        var school_begin = {'01': '101', '02': '201', '03': '301'};
+        var school_end = {'01': '117', '02': '205', '03': '309'};
         var school = {
             '101': "北京大学",
             '102': "北京工商大学",
@@ -46,9 +47,17 @@ $(function() {
             '205': "天津城建大学",
             '206': "中国民航大学",
             '207': "天津师范大学津沽分校",
-            '208': "河北工业大学"
+            '208': "河北工业大学",
+            '301': '西安外国语大学',
+            '302': '',
+            '303': '',
+            '304': '',
+            '305': '',
+            '306': '',
+            '307': '',
+            '308': '',
+            '309': ''
         };
-
         for (var i = school_begin[area]; i < school_end[area]; i++) {
             $("select[name=school]").append("<option>" + school[i] + "</option>");
         }
@@ -59,7 +68,7 @@ $(function() {
         $('div#school').children().remove();
 
         var area = $("select[name=area]").val();
-        var turn = { '01': 16, '02': 8, '03': 8, '04': 5 };
+        var turn = {'01': 16, '02': 8, '03': 8, '04': 5};
         var times;
         if (turn[area] % 2 == 0) {
             times = turn[area] / 2;
@@ -68,7 +77,7 @@ $(function() {
         }
         //添加得分选项、最佳辩手、学校
         for (var i = 0; i < times; i++) {
-            $('div#school').append("<ul><a>第" + NumberToChinese(i + 1) + "场:</a><select name='score_a" + i + "'></select><select name='score_b" + i + "'></select><p><a id='bestName'> 最佳辩手:</a><input type='text' name='bestName'></p></ul>");
+            $('div#school').append("<ul><a>第" + NumberToChinese(i + 1) + "场:</a><br><select name='score_a" + i + "'></select><select name='score_b" + i + "'></select><p id='bestName-all'><a id='bestName'> 最佳辩手:</a><input type='text' name='bestName'></p></ul>");
         }
         $("select[name=school]").remove();
         $('select[name^=score]').before("<select name='school'>");
@@ -87,66 +96,66 @@ $(function() {
             $("select[name=score_a9], select[name = score_b9]").append(" <option> " + j + " </option> ");
 
             //改前面积分后面跟着变动
-            $("select[name=score_a0]").change(function() {
+            $("select[name=score_a0]").change(function () {
                 $("select[name=score_b0]").val(9 - $("select[name=score_a0]").val());
             });
-            $("select[name=score_a1]").change(function() {
+            $("select[name=score_a1]").change(function () {
                 $("select[name=score_b1]").val(9 - $("select[name=score_a1]").val());
             });
-            $("select[name=score_a2]").change(function() {
+            $("select[name=score_a2]").change(function () {
                 $("select[name=score_b2]").val(9 - $("select[name=score_a2]").val());
             });
-            $("select[name=score_a3]").change(function() {
+            $("select[name=score_a3]").change(function () {
                 $("select[name=score_b3]").val(9 - $("select[name=score_a3]").val());
             });
-            $("select[name=score_a4]").change(function() {
+            $("select[name=score_a4]").change(function () {
                 $("select[name=score_b4]").val(9 - $("select[name=score_a4]").val());
             });
-            $("select[name=score_a5]").change(function() {
+            $("select[name=score_a5]").change(function () {
                 $("select[name=score_b5]").val(9 - $("select[name=score_a5]").val());
             });
-            $("select[name=score_a6]").change(function() {
+            $("select[name=score_a6]").change(function () {
                 $("select[name=score_b6]").val(9 - $("select[name=score_a6]").val());
             });
-            $("select[name=score_a7]").change(function() {
+            $("select[name=score_a7]").change(function () {
                 $("select[name=score_b7]").val(9 - $("select[name=score_a7]").val());
             });
-            $("select[name=score_a8]").change(function() {
+            $("select[name=score_a8]").change(function () {
                 $("select[name=score_b8]").val(9 - $("select[name=score_a8]").val());
             });
-            $("select[name=score_a9]").change(function() {
+            $("select[name=score_a9]").change(function () {
                 $("select[name=score_b9]").val(9 - $("select[name=score_a9]").val());
             });
 
             //改后面积分前面跟着变动
-            $("select[name=score_b0]").change(function() {
+            $("select[name=score_b0]").change(function () {
                 $("select[name=score_a0]").val(9 - $("select[name=score_b0]").val());
             });
-            $("select[name=score_b1]").change(function() {
+            $("select[name=score_b1]").change(function () {
                 $("select[name=score_a1]").val(9 - $("select[name=score_b1]").val());
             });
-            $("select[name=score_b2]").change(function() {
+            $("select[name=score_b2]").change(function () {
                 $("select[name=score_a2]").val(9 - $("select[name=score_b2]").val());
             });
-            $("select[name=score_b3]").change(function() {
+            $("select[name=score_b3]").change(function () {
                 $("select[name=score_a3]").val(9 - $("select[name=score_b3]").val());
             });
-            $("select[name=score_b4]").change(function() {
+            $("select[name=score_b4]").change(function () {
                 $("select[name=score_a4]").val(9 - $("select[name=score_b4]").val());
             });
-            $("select[name=score_b5]").change(function() {
+            $("select[name=score_b5]").change(function () {
                 $("select[name=score_a5]").val(9 - $("select[name=score_b5]").val());
             });
-            $("select[name=score_b6]").change(function() {
+            $("select[name=score_b6]").change(function () {
                 $("select[name=score_a6]").val(9 - $("select[name=score_b6]").val());
             });
-            $("select[name=score_b7]").change(function() {
+            $("select[name=score_b7]").change(function () {
                 $("select[name=score_a7]").val(9 - $("select[name=score_b7]").val());
             });
-            $("select[name=score_b8]").change(function() {
+            $("select[name=score_b8]").change(function () {
                 $("select[name=score_a8]").val(9 - $("select[name=score_b8]").val());
             });
-            $("select[name=score_b9]").change(function() {
+            $("select[name=score_b9]").change(function () {
                 $("select[name=score_a9]").val(9 - $("select[name=score_b9]").val());
             });
         }
@@ -154,10 +163,12 @@ $(function() {
         $("a#bestName").after("<select name='bestName'>");
         for (var i = 1; i < 5; i++) {
             $("select[name=bestName]").append("<option>正方" + NumberToChinese(i) + "辩");
-        };
+        }
+        ;
         for (var i = 1; i < 5; i++) {
             $("select[name=bestName]").append("<option>反方" + NumberToChinese(i) + "辩");
-        };
+        }
+        ;
     }
 
     //数字转汉字
@@ -219,40 +230,39 @@ $(function() {
 
 });
 //div#finish
-$(function() {
+$(function () {
     var a = $("div#finish");
-
     a.append("<a>请选择");
 
-    $("select[name=area],select[name=turn]").change(areaFinish);
-    //$("select[name=school],select[name^=score],select[name=bestName],input[name=bestName]").live('change', areaFinish);
-    $(document).on('change', 'select[name=school],select[name^=score],select[name=bestName],input[name=bestName]', areaFinish);
+    //$("select,input[name=bestName]").live('change', areaFinish);
+    $(document).on('change', 'select,input[name=bestName]', areaFinish);
 
     function areaFinish() {
         a.children().remove();
         //赛区
         var text_area = $("select[name=area]").find("option:selected").text();
+        var year = $("select[name=year]").find("option:selected").text();
         var val_area = $("select[name=area]").find("option:selected").val();
-        a.append('<a id="area">' + text_area + '赛区</a><br>');
+        a.append('<a class="area">' + text_area + '赛区' + year + '赛季' + '</a><br>');
 
         //轮次
         var text_turn = $("select[name=turn]").find("option:selected").text();
-        a.append('<a id="turn">' + text_turn + '</a><br>');
+        a.append('<a class="turn">' + text_turn + '</a><br>');
 
         //修改背景色¥
-        var areaBackground = { "01": "blue", "02": "purple" };
+        var areaBackground = {"01": "blue", "02": "purple"};
         a.css("background", areaBackground[val_area]);
 
         //修改学校、积分、佳辩
         var school = $("select[name=school]").find("option:selected");
         var score = $("select[name^=score]").find("option:selected");
         for (var i = 0; i < school.length; i++) {
-            a.append('<a id="school">' + school[i].innerText + '</a>');
+            a.append('<a class="school">' + school[i].innerText + '</a>');
 
             if (i % 2 == 0) {
                 for (var k = i; k < i + 2; k++) {
-                    a.append('<a id="score">' + score[k].innerText + '</a>');
-                    if (k % 2 == 0) a.append('<a id="score">:</a>');
+                    a.append('<a class="score">' + score[k].innerText + '</a>');
+                    if (k % 2 == 0) a.append('<a class="score">:</a>');
                 }
             }
             if (i % 2 != 0) {
@@ -267,12 +277,108 @@ $(function() {
         var name = $('input[name=bestName]');
         for (j = 0; j < name.length; j++) {
             if (best[j].value) {
-                $(b[j]).append('<a id="name">最佳辩手:' + best[j].value + '</a>');
+                $(b[j]).append('<a class="name">最佳辩手:' + best[j].value + '</a>');
             }
             if (name[j].value) {
-                $(b[j]).append('<a id="name">' + name[j].value + '</a>');
+                $(b[j]).append('<a class="name">' + name[j].value + '</a>');
             }
         }
 
     }
 });
+
+//把佳辩隐藏掉，计算积分榜
+$(function () {
+    //把佳辩隐藏掉
+    $('select').change(function () {
+        if ($('a.name,#bestName-all')) $('a.name,#bestName-all').remove();
+    });
+
+    //用indexedDB 存储数据
+    if (!indexedDB) {
+        alert("你的浏览器不支持积分榜功能，请使用Chrome、Firefix等主流浏览器、或将IE升级到10以上")
+    } else {
+        //TODO
+    }
+
+    function setIndexedDB() {
+        //参数1为数据库名称，格式为字符串
+        //参数2为版本，大于1的正整数
+        var openRequest = indexedDB.open("test");
+        var db;
+
+        // success：打开成功// error：打开失败// blocked：上一次的数据库连接还未关闭
+        // upgradeneeded：第一次打开该数据库，或者数据库版本发生变化。
+
+        openRequest.ongradeneeded = function (e) {
+            console.log("Upgrading...");
+        };
+
+        openRequest.onerror = function (e) {
+            console.log("Error:" + e.target.errorCode);
+            console.dir(e);
+        };
+
+        openRequest.onsuccess = function (e) {
+            db = e.target.result;
+            if (!db.objectStoreNames.contains("2016")) {
+                db.createObjectStore("2016", {keyPath: "school"});
+            }
+        };
+
+    }
+});
+
+function setData() {
+    //收集数据
+    var area = $("select[name = area]>option:selected").text();
+    var year = $("select[name = year]>option:selected").text();
+    var turn = $("select[name = turn]>option:selected").text();
+
+    var school = $("select[name=school]>option:selected");
+    var score = $("select[name^=score]>option:selected");
+    var data = {area: area, year: year, turn: turn};
+    var dataValue = 0;
+    for (var i = 0; i < school.length; i++) {
+        Object.defineProperty(data, school[i].text, {value: score[i].text, enumerable: true, configurable: true});
+        dataValue += parseInt(data[school[i].text]);
+    }
+
+    // if (dataValue == school.length * 9 / 2 || Object.keys(data).length == school.length + 3)
+    //     return data;
+    // else
+    //     alert("输入的学校重复或积分有误，请检查");
+
+}
+//TODO
+function boardDgl() {
+
+    //计算积分榜 最外层缺一个遍历
+    if (data.area) {
+
+    }
+}
+
+function setDB() {
+    // var data = {"area":"北京","year":"2016","turn":"第一轮","北京理工大学":"1","北京大学":"0","北京交通大学":"6","外交学院":"3","对外经贸大学":"2","北京工业大学":"7"};
+    var openRequest = indexedDB.open("DGL-score");
+    var db;
+
+    openRequest.onupgradeneeded = function (e) {
+        var result = e.target.result;
+        result.createObjectStore("score", {autoIncrement: true});
+    };
+
+    openRequest.onsuccess = function (e) {
+        db = e.target.result;
+        putDB;
+    };
+}
+function putDB() {
+    data={1:"test",2:"asda",3:"asdads"};
+    var trans = db.transaction(["score"], "readwrite");
+    var store = trans.objectStore("score");
+    store.add(data);
+    // store.put(setData());
+}
+setDB();
