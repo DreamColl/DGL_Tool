5e41f907113e8567f2b631c957d295ae6d030bc4
xiongjiyuan@i-vsun.com <xiongjiyuan@i-vsun.com>
2016-10-24 17:57:30 +0800

    v1.1接入indexedDB数据库

--- Tool_score/DGL-Test/compatibile.js
+++ Tool_score/DGL-Test/compatibile.js
@@ -16,22 +16,22 @@ window.onload = function () {
                     (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
 
 //以下进行测试
-    if (Sys.ie) brower.innerText = 'IE: ' + Sys.ie;
+    if (Sys.ie) brower.innerText = 'IE: ' + Sys.ie +'程序员最讨厌的浏览器';
     if (Sys.firefox) brower.innerText = 'Firefox: ' + Sys.firefox;
-    if (Sys.chrome) brower.innerText = 'Chrome: ' + Sys.chrome;
+    if (Sys.chrome) brower.innerText = 'Chrome: ' + Sys.chrome +'世界上最好的浏览器';
     if (Sys.opera) brower.innerText = 'Opera: ' + Sys.opera;
     if (Sys.safari) brower.innerText = 'Safari: ' + Sys.safari;
 
-//WebSQL
+//WebSQL（这货的兼容性有毒）
     var db = openDatabase('testDB', '1.0', 'Test DB', 2 * 1024 * 1024);
     if (db != null)
         document.getElementById("result-WebSQL").innerHTML = ("当前浏览器支持 Web SQL Database");
     else
-        document.getElementById("result-WebSQL").innerHTML = ("当前浏览器不支持 Web SQL Database");
+        document.getElementById("result-WebSQL").innerHTML = ("当前浏览器不支持 Web SQL Database，赶快下载一个chrome吧");
 
-//IndexedDB
+//IndexedDB（浏览器最新版本均可兼容）
     if (indexedDB)
         document.getElementById("result-IndexedDB").innerHTML = ("当前浏览器支持 indexedDB");
     else
-        document.getElementById("result-IndexedDB").innerHTML = ("当前浏览器不支持 indexedDB");
+        document.getElementById("result-IndexedDB").innerHTML = ("当前浏览器不支持 indexedDB，请把你的IE或者Safari升级到10，或者下载一个chrome吧");
 };
